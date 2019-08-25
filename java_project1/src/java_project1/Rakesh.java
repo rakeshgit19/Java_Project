5a382a2b5c18c300bcb8efa152bd5be4d8b0400e
rakeshgit19 <rakeshiiet2010@gmail.com>
2019-08-25 17:14:08 +0530

    Now i changed it

---
+++ java_project1/src/java_project1/Rakesh.java
@@ -0,0 +1,18 @@
+package java_project1;
+
+public class Rakesh 
+{
+
+	public static void main(String[] args)
+	{
+		int x=10;
+		int y=20;
+		int z;
+		System.out.println(z=x+y);;
+		
+		
+		
+
+	}
+
+}
